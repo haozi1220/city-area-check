b354762ebed4ebc5f025615e5c0eb0ea58b98df8
haozi1220 <634930118@qq.com>
2018-11-04 17:01:06 +0800

    fuzzy serch is done

--- city-checkgit/README.md
+++ city-checkgit/README.md
@@ -19,3 +19,16 @@ npm run build --report
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+## 实现思路：
+```
+    第一步：拿到后台数据，对后台进行解析拿到城市名数据
+    第二步：安装汉字拼音转换文件js-pinyin,
+        ```
+            1.运行npm install js-pinyin --save-dev下载安装依赖
+            2.在main.js里添加引用，注册为全局引用
+            3.在组件里通过“this.$pinyin.方法”方式调用pinyinjs的方法
+        ```
+    第三步：将每一个城市转化为一个对象，添加pinyin、firstLetter并赋值,
+    第四步：利用正则匹配不同输入内容，解析出不同城市内容并渲染页面。
+```
--- city-checkgit/src/components/HelloWorld.vue
+++ city-checkgit/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="hello">
-    <h2>地区性选择</h2>
-    <div class="city_check_wrapp">
+	<div class="hello">
+		<h2>地区性选择</h2>
+		<div class="city_check_wrapp">
 		<span>请输入城市</span><input type="text" v-model="cityInput">
 	</div>
 	<div class="city_wrapp" v-if="!isNewVal">
@@ -20,103 +20,126 @@
 			</div>
 		</div>
 	</div>
-    <div class="city_list_wrap" v-if="isNewVal">
-        我是诚实列表
-    </div>
-  </div>
+		<div class="city_list_wrap" v-if="isNewVal">
+			<ul class="filter_list">
+				<li v-for="item in filterCityList" @click="checkCityName(item.name)">
+					<span>{{item.name}}</span><span>{{item.pinyin}}</span>
+				</li>
+			</ul>
+		</div>
+	</div>
 </template>
 
 <script>
-
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-  		cityData: null,
-  		cityItemData:null,
-  		cityInput:null,
-      isNewVal:false,
-      cityList:[]
-    }
-  },
-  created () {
-    let that = this;
-    // console.log(this.$pinyin.getFullChars('管理员'))  提取拼音并且首字母大写
-    // console.log(this.$pinyin.getCamelChars('管理员'))  提取首字母并大写
-    // console.log(this.$pinyin.getCamelChars('1234'))
-    // console.log(this.$pinyin.getCamelChars('english'))
-	  let url = '../../static/data/city.json';
-      this.$axios.get(url)
-      .then(res => {
-          let getCityData = res.data[267040].value.cityArray;
-          this.cityData = getCityData;
-          this.cityItemData = this.cityData[0].tabdata;
-          let newCityData = this.cityData.slice(1);
-          newCityData.forEach(function(item){
-            item.tabdata.forEach(function(item){
-              item.dd.forEach(function(item){
-                that.cityList.push(item.cityName);
-              })
-            })
-          })
-      }).catch(error => {
-          console.log(error);
-      });
-  },
-  watch:{
-    cityInput: function(newVal,oldVal){
-        if(newVal.length&&newVal.length > 0){
-            var re = /[A-Za-z]+$/;
-            var re2 = /^[/\u4e00-\u9fa5]$/;
-
-            if(re.test(newVal)){
-               // do nothing
-            }else if(re2.test(newVal) || newVal != oldVal){
-              let citylist = [];
-              for(let a = 0; a < this.cityList.length; a++){
-                if(this.cityList[a].indexOf(newVal)){
-                  cityList.push(this.cityList[a]);
-                }
-              }
-              console.log(cityList);
-            }
-            this.isNewVal = true;
-        }else {
-            this.isNewVal = false;
-        }
-    }
-  },
-  methods:{
-	  getCityList:function(val) {
-		  let that = this;
-		  that.cityData.forEach(function(item,index){
-			  if(val == item.tabname){
-			    that.cityItemData = item.tabdata;
-			  };
-		  })
-	  },
-	  inputCityName:function(e){
-			this.cityInput = e.cityName;
-	  },
-  }
-}
+	export default {
+		name: 'HelloWorld',
+		data () {
+			return {
+				cityData: null,
+				cityItemData:null,
+				cityInput:null,
+				isNewVal:false,
+				cityList:[],
+				filterCityList:[]
+			}
+		},
+		created () {
+			let that = this;
+			// console.log(this.$pinyin.getFullChars('管理员'))  提取拼音并且首字母大写
+			// console.log(this.$pinyin.getCamelChars('管理员'))  提取首字母并大写
+			// console.log(this.$pinyin.getCamelChars('1234'))
+			// console.log(this.$pinyin.getCamelChars('english'))
+			let url = '../../static/data/city.json';
+			this.$axios.get(url)
+			.then(res => {
+				let getCityData = res.data[267040].value.cityArray;
+				this.cityData = getCityData;
+				this.cityItemData = this.cityData[0].tabdata;
+				let newCityData = this.cityData.slice(1);
+				newCityData.forEach(function(item){
+					item.tabdata.forEach(function(item){
+						item.dd.forEach(function(item){
+							let cityInfo = {};
+							cityInfo.name = item.cityName;
+							cityInfo.pinyin = that.$pinyin.getFullChars(item.cityName).toLowerCase();
+							cityInfo.firstLetter = that.$pinyin.getCamelChars(item.cityName);
+							that.cityList.push(cityInfo);
+						})
+					})
+				})
+			}).catch(error => {
+					console.log(error);
+			});
+		},
+		watch:{
+			cityInput: function(newVal,oldVal){
+				var re = /^[A-Z]+$/; //大字首字母
+				var pinyinReg = /^[a-z]+$/;//拼音搜索
+				// var re2 = /^[/\u4e00-\u9fa5]$/;
+				var allCityArr = [];
+				if(this.cityInput){
+					console.log(111);
+					var testStr = String(this.cityInput);
+					if(re.test(testStr)){
+						for(let i = 0; i < this.cityList.length; i++){
+							if(this.cityList[i].firstLetter.indexOf(testStr)>=0){
+								allCityArr.push(this.cityList[i]);
+							}
+						}
+					} else if( pinyinReg.test(testStr) ){
+						for(let m = 0; m < this.cityList.length; m++){
+							if(this.cityList[m].pinyin.indexOf(testStr)>=0){
+								allCityArr.push(this.cityList[m]);
+							}
+						}
+					}else {
+						for(let a = 0; a < this.cityList.length; a++){
+							if(this.cityList[a].name.indexOf(testStr)>=0){
+								allCityArr.push(this.cityList[a]);
+							}
+						}
+					}
+					this.filterCityList = allCityArr;
+					this.isNewVal = true;
+				}else {
+						this.isNewVal = false;
+				}
+			}
+		},
+		methods:{
+			getCityList:function(val) {
+				let that = this;
+				that.cityData.forEach(function(item,index){
+					if(val == item.tabname){
+						that.cityItemData = item.tabdata;
+					};
+				})
+			},
+			inputCityName:function(e){
+				this.cityInput = e.cityName;
+			},
+			checkCityName:function(e){
+				this.cityInput = e;
+			}
+		}
+	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
-  font-weight: normal;
+	font-weight: normal;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+	list-style-type: none;
+	padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+	display: inline-block;
+	margin: 0 10px;
 }
 a {
-  color: #42b983;
+	color: #42b983;
 }
 .citi_list {
 	width: 430px;
